c90d6e698945e0279a2bcebc17e067b7a78a3fc2
sagarssabat <sagarssabat@gmail.com>
2017-04-10 17:11:26 +0530

    about page added to check the state

--- js/app.js
+++ js/app.js
@@ -3,22 +3,19 @@
      angular.module('myApp', ['ui.router'])
          .config(function ($stateProvider, $urlRouterProvider, $httpProvider, $locationProvider) {
              // for http request with session
-             $httpProvider.defaults.withCredentials = true;
-
+             //  $httpProvider.defaults.withCredentials = true;
+             var tempateURL = "views/template/template.html"; //Default Template URL
              $stateProvider
                  .state('home', {
                      url: "/",
                      templateUrl: "views/content/home.html",
+
                      controller: 'homeCtrl'
                  })
-                 //  .state('form', {
-                 //      url: "/form",
-                 //      templateUrl: "views/template.html",
-                 //      controller: 'FormCtrl'
-                 //  })
+
                  .state('about', {
                      url: '/about',
-                     templateUrl: 'views/template.html',
+                     templateUrl: 'views/content/about.html',
                      controller: 'abtCtrl'
                  });
 
@@ -27,3 +24,40 @@
              $locationProvider.html5Mode(true);
          });
  })();
+
+
+
+
+
+ // // JavaScript Document
+ // var myApp = angular.module('myApp', [
+ //     'ui.router',
+ //     'about.controller.js',
+ //     'home.controller.js',
+ //     'templateservice.js'
+
+ // ]);
+
+ // myApp.config(function ($stateProvider, $urlRouterProvider, $httpProvider, $locationProvider) {
+ //     // for http request with session
+ //     $httpProvider.defaults.withCredentials = true;
+ //     $stateProvider
+ //         .state('home', {
+ //             url: "/",
+ //             templateUrl: "views/content/home.html",
+ //             controller: 'homeCtrl'
+ //         })
+ //         .state('about', {
+ //             url: "/about",
+ //             templateUrl: "views/content/about.html",
+ //             controller: 'abtCtrl'
+ //         });
+ //     $urlRouterProvider.otherwise("/");
+ //     $locationProvider.html5Mode(isproduction);
+ // });
+
+ // firstapp.config(function ($translateProvider) {
+ //     $translateProvider.translations('en', LanguageEnglish);
+ //     $translateProvider.translations('hi', LanguageHindi);
+ //     $translateProvider.preferredLanguage('en');
+ // });
